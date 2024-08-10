ef43507e6a8891024f5c16965772ae1f7f633b6d
Wilmarie <wguzmor@gmail.com>
2024-08-09 22:10:37 -0400

    edited name vs title

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
 const initialCards = [
   {
-    title: "Yosemite Valley",
+    name: "Yosemite Valley",
     Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
 
   {
-    title: "Lake Louise",
+    name: "Lake Louise",
     Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
 
   {
-    title: "Bald Mountains",
+    name: "Bald Mountains",
     Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
 
   {
-    title: "Latemar",
+    name: "Latemar",
     Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
 
   {
-    title: "Vanoise National Park",
+    name: "Vanoise National Park",
     Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
 
   {
-    title: "Lago di Braies",
+    name: "Lago di Braies",
     Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
