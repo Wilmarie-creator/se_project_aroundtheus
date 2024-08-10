01ec87904baba2f931d5f2dd8164dd1f36942132
Wilmarie <wguzmor@gmail.com>
2024-08-09 22:00:55 -0400

    card arrays added

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const initialCards = [
+  {
+    title: "Yosemite Valley",
+    Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+
+  {
+    title: "Lake Louise",
+    Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+
+  {
+    title: "Bald Mountains",
+    Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+
+  {
+    title: "Latemar",
+    Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+
+  {
+    title: "Vanoise National Park",
+    Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+
+  {
+    title: "Lago di Braies",
+    Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+console.log(initialCards);
